7d0e440433f0f5ab7a9fadb3c41e3818617a0a33
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-01-21 13:12:56 -0400

    🎨 ((home)): add home view and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
+		<html lang='es'>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<title>La Tienda 🪙</title>
+			<head />
+			<body className='text-[#FFFCF7] min-h-screen bg-[#141717] flex flex-col gap-10 items-center content-center relative overflow-hidden'>
+				{children}
+			</body>
+		</html>
+	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,57 @@
+'use client'
+
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import Tile from '../components/Tile'
+
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
-  );
+	const [gifts, setGifts] = useState<any[]>([])
+
+	useEffect(() => {
+		fetch('http://localhost:3000/api/')
+			.then(res => res.json())
+			.then(res => setGifts(res))
+	}, [])
+
+	return (
+		<main>
+			<div>
+				<div className='absolute w-[1158px] h-[1158px;] -translate-x-1/2 -translate-y-[747px] bg-gradient-radial from-[#815CD0]/40  via-[#815CD0]/10 to-transparent left-1/2  rounded-full blur-3xl opacity-70 '></div>
+
+				<div className='flex flex-col items-center py-5'>
+					<h1 className='text-7xl font-bold'>La Tienda</h1>
+					<h2 className='text-4xl font-semibold'>Canjea los puntos en premios.</h2>
+				</div>
+
+				<div className='flex justify-between relative w-1/5 bg-[#1B1F1F] py-2 px-4 rounded-lg'>
+					<input
+						type='text'
+						className='font-medium bg-transparent focus-visible:outline-0 border-0'
+						placeholder='Buscar...'
+					/>
+					<button>
+						<Image
+							src={'/search.png'}
+							width={20}
+							height={20}
+							alt='search-icon'
+							className='absolute top-2 right-3'
+						/>
+					</button>
+				</div>
+
+				<div className='grid lg:grid-cols-3 md:grid-cols-2 gap-[40px]'>
+					{gifts &&
+						gifts.map(gift => (
+							<Tile
+								key={gift.name}
+								title={gift.name}
+								description={gift.description}
+								price={gift.price}
+							/>
+						))}
+				</div>
+			</div>
+		</main>
+	)
 }
