cb4b271481e6f836de5dbfbd6b1dd1552b7b08e3
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-01-22 09:29:59 -0400

    ♻️ change title to head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-			<title>La Tienda 🪙</title>
 			<head />
 			<body className='text-[#FFFCF7] min-h-screen bg-[#141717] flex flex-col gap-10 items-center content-center relative overflow-hidden'>
 				{children}
