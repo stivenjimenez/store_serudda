1b6dfbbc6ec8100959ad3d27f0568a951f85c5e0
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-01-22 12:48:06 -0400

    🎨 add maxlength to the input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,9 +8,17 @@ export default function SearchBar() {
 				className='font-semibold w-96 text-white bg-[#1B1F1F] py-2 px-3 rounded-lg focus:ring-[#815CD0] focus:ring-2 focus-visible:outline-0 border-0 ease-in-out duration-100'
 				placeholder='Buscar...'
 				autoComplete='off'
+				maxLength={40}
 			/>
 			<button className='absolute top-2 right-4'>
-				<Image src={'/search.png'} width={22} height={22} alt='search-icon' priority />
+				<Image
+					src={'/search.png'}
+					width={22}
+					height={22}
+					alt='search-icon'
+					className='bg-[#1B1F1F]'
+					priority
+				/>
 			</button>
 		</>
 	)
