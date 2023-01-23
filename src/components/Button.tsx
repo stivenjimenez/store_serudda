6b7a70d6dfe552c43191ffac7ac013cc9377bedd
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-01-23 09:23:24 -0400

    🎨 change button font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import type { IButton } from '@/models/Button'
 
 export default function Button({ text }: IButton) {
 	return (
-		<button className='w-[144px] h-[40px] bg-purple rounded-[10px] hover:bg-purple/secondary ease-in-out duration-300'>
+		<button className='w-[144px] h-[40px] bg-purple rounded-[10px] hover:bg-purple/secondary ease-in-out duration-300 font-medium'>
 			{text}
 		</button>
 	)
