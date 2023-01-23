3c1f524656cbd917ca91a2f46ee6a6b35ded8ed3
Santiago Hernández <68081080+santiagohernandezs@users.noreply.github.com>
2023-01-23 10:09:56 -0400

    🎨 add animations to tile

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 export default function Tile({ title, description, price }: ITile): JSX.Element {
 	return (
-		<div className='w-[20rem] h-[20rem] rounded-xl bg-black/primary py-[35px] px-[30px] flex flex-col justify-between hover:shadow-2xl ease-in-out duration-300'>
+		<div className='w-[20rem] h-[20rem] rounded-xl bg-black/primary py-[35px] px-[30px] flex flex-col justify-between hover:-translate-y-1 hover:shadow-2xl ease-in-out duration-150'>
 			<h3 className='text-3xl font-bold truncate text-ellipsis'>{title}</h3>
 			<p className='text-2sm  h-max mb-auto my-[14px] text-justify line-clamp-3 break-words antialiased'>
 				{description}
